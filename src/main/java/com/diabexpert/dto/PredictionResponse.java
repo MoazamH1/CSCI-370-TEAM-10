d5819d21557dab964a8563956ffffc1352106b1e
MoazamH1 <moazamh123@gmail.com>
2025-05-14 23:00:36 -0400

    Only Frontend

--- src/main/java/com/diabexpert/dto/PredictionResponse.java
+++
@@ -1,9 +0,0 @@
-package com.diabexpert.dto;
-
-public class PredictionResponse {
-    public String severity;
-
-    public PredictionResponse(String severity) {
-        this.severity = severity;
-    }
-}
